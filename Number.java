bda65b32cb9ff3d71fee6183fda8e3399ae2c953
Anuhari <anukesavamoorthy@gmail.com>
2017-06-07 17:08:09 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Number
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n==0)
+{
+System.out.println("The number is Zero");
+}
+if(n>0)
+{
+System.out.println("The number is greater than Zero");
+}
+if(n<0)
+{
+System.out.println("The number is  less than Zero");
+}
+}
+}
